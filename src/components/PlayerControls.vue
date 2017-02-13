51db83ae6e4744f99ae58138c6bc1a834f587078
asghol <Asgeir.Holleland@knowit.no>
2017-02-13 07:02:47 +0100

    Added PlayerControls which should have been added several commits ago

---
+++ src/components/PlayerControls.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <button :disabled="!isLoaded" v-on:click="togglePlay();">{{ playbackButtonText }}</button>
+    <button :disabled="!isLoaded" v-on:click="stopPlayback();">Stop</button>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'player-controls',
+    props: ['currentChannel', 'isPlaying', 'isLoaded'],
+    data: function () {
+      return {
+        disabled: true
+      }
+    },
+    computed: {
+      playbackButtonText: function () {
+        return this.isPlaying ? 'Pause' : 'Play'
+      }
+    },
+    methods: {
+      togglePlay: function () {
+        this.$emit('play', !this.isPlaying)
+      },
+      stopPlayback: function () {
+        this.$emit('stop')
+      }
+    }
+  }
+</script>
+
+<style scoped>
+</style>
